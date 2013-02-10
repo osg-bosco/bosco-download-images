e99ac6f4ebd271314432ad3e9730f57f3c3d0eeb
Derek Weitzel <djw8605@gmail.com>
2013-02-09 22:23:43 -0600

    Adding css and javascript

---
+++ js/bosco-download.js
@@ -0,0 +1,17 @@
+
+
+var $ = document; // shortcut
+var cssId = 'myCss';  // you could encode the css path itself to generate id..
+if (!$.getElementById(cssId))
+{
+    var head  = $.getElementsByTagName('head')[0];
+    var link  = $.createElement('link');
+    link.id   = cssId;
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'http://website.com/css/stylesheet.css';
+    link.media = 'all';
+    head.appendChild(link);
+}
+
+
